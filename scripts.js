2e3c8a3b49b7878799b986c3c4df76f4be807540
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-04-09 21:07:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,4 +16,21 @@ let clock = () => {
 	setTimeout(clock, 1000);
 }
 
-clock();
+let clock12 = () => {
+	let date = new Date();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+
+	let time_period = "";
+	time_period = hours < 12 ? "AM" : "PM";
+	time_period = hours < 12 ? hours - 12 : hours;
+
+	hours = hours < 10 ? `0${hours}` : hours;
+	minutes = minutes < 10 ? `0${minutes}` : minutes;
+	seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+	let time = `${hours}:${minutes}:${seconds}`
+	document.getElementById("clock12").innerHTML = time + ":" + time_period;
+	setTimeout(clock, 1000);	
+}
