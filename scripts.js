2d852a7dedf0d63bb164836a286d1edc2538180c
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-04-09 22:48:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ let clock = () => {
 }
 
 let clock12 = () => {
-	let date = new Date();
+	let date = ne Date();
 	let hours = date.getHours();
 	let minutes = date.getMinutes();
 	let seconds = date.getSeconds();
