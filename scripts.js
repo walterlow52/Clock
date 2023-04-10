a465f842defdad08090a3d240cc83e995c198792
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-04-09 22:11:23 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -23,8 +23,7 @@ let clock12 = () => {
 	let seconds = date.getSeconds();
 
 	let time_period = "";
-	time_period = hours < 12 ? "AM" : "PM";
-	time_period = hours < 12 ? hours = hours - 12 : hours;
+	time_period = hours < 12 ? (hours - 12) + "AM" : hours + "PM";
 
 	hours = hours < 10 ? `0${hours}` : hours;
 	minutes = minutes < 10 ? `0${minutes}` : minutes;
