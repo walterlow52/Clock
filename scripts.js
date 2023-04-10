cef9724e65b7abc3d6c42107c165085371aa59b1
walterlow52 <79463731+walterlow52@users.noreply.github.com>
2023-04-09 22:29:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,21 +17,21 @@ let clock = () => {
 }
 
 let clock12 = () => {
-	let date12 = new Date();
-	let hours12 = date12.getHours();
-	let minutes12 = date12.getMinutes();
-	let seconds12 = date12.getSeconds();
+	let date = new Date();
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
 
-	let time_period12 = "";
-	time_period12 = hours12 < 12 ? "AM" : "PM";
+	let time_period = "";
+	time_period = hours < 12 ? "AM" : "PM";
 
-	hours12 = hours12 > 12 ? hours12 - 12 : hours12;
-	hours12 = hours12 < 10 ? `0${hours12}` : hours12;
-	minutes12 = minutes12 < 10 ? `0${minutes12}` : minutes12;
-	seconds12 = seconds12 < 10 ? `0${seconds12}` : seconds12;
+	hours = hours > 12 ? hours - 12 : hours;
+	hours = hours < 10 ? `0${hours}` : hours;
+	minutes = minutes < 10 ? `0${minutes}` : minutes;
+	seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-	let time12 = `${hours12}:${minutes12}:${seconds12}`
-	document.getElementById("clock12").innerHTML = time12 + ":" + time_period12;
+	let time = `${hours}:${minutes}:${seconds}`
+	document.getElementById("clock12").innerHTML = time + ":" + time_period;
 	setTimeout(clock12, 1000);	
 }
 
